581f071f4cac374c05cdef5f8b8f4289265eb0a2
diatomicC <emily.hur.21@gmail.com>
2023-04-06 10:22:13 +0800

    updates

--- script.js
+++ script.js
@@ -1,76 +1,87 @@
-function convert() {
-  const input = document.querySelector('#input').value;
-  if (input.trim() === '') {
-    return;
+function search() {
+  const api_key = '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'
+  const keywords = document.getElementById('chat-input').value
+  const messages = [
+{ role: 'system', content: 'You are a LaTeX expert.' },
+{ role: 'user', content: keywords + 'change this to LaTeX form. Only print the LaTeX equaiton.' },
+]
+  const config = {
+    headers: {
+      Authorization: `Bearer ${api_key}`,
+      'Content-Type': 'application/json',
+    },
+  }
+  const data = {
+    model: 'gpt-3.5-turbo',
+    temperature: 0.5,
+    n: 1,
+    messages: messages,
   }
 
-  // Call GPT API to convert input text into LaTeX
-  const latex = getLatex(input);
+  axios
+    .post('https://api.openai.com/v1/chat/completions', data, config)
+    .then(function (response) {
+      let resultDiv = document.getElementById('chat-window')
+      resultDiv.innerHTML = ''
+      resultDiv.innerHTML += `
+        <div class="message-container user-message">
+          <p>${keywords}</p>
+        </div>
+      `
+        response.data.choices.forEach(function (choice, index) {
+          
+          //$와 $ 사이에 있는 값만 latexContent 에 저장
+          const regex = /\$([^$]+)\$/g;
+          const matches = choice.message.content.match(regex);
+          const latexContent = [];
 
-  // Call Math API to convert LaTeX into an image file
-  const url = 'https://api.mathpix.com/v3/text';
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer YOUR_API_KEY_HERE'
-  };
-  const body = JSON.stringify({
-    "format": "png",
-    "data": latex,
-    "transforms": ["mathml"]
-  });
-  fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: body
-  }).then(response => {
-    return response.json();
-  }).then(data => {
-    const image_url = data?.images?.[0]?.url;
-    if (image_url) {
-      // Display the image and download button
-      const output = document.querySelector('#output');
-      output.innerHTML = `<img src="${image_url}" alt="Mathematical expression image"><br><button onclick="download()">Download</button>`;
-      output.style.display = 'block';
-    } else {
-      alert('Image conversion failed.');
-    }
-  }).catch(error => {
-    console.error(error);
-    alert('Error occurred while converting image.');
-  });
+          for (let i = 0; i < matches.length; i++) {
+            // Extract the value between the dollar symbols
+            const value = matches[i].replace(/\$/g, "");
+            latexContent.push(value);
+          }
+
+          
+          if (latexContent) {
+            resultDiv.innerHTML += 
+            `<div class="message-container system-message" id="latexresult">
+              $${latexContent}$
+              <button id="copy-btn" onclick="copyToClipboard()">Copy</button>
+            </div>`
+          } else {
+            resultDiv.innerHTML += 
+            `<div class="message-container system-message" id="latexresult">
+              We cannot provide LaTeX format for following equation. Please enter again.
+            </div>`
+          }
+
+          //디버깅용
+
+          // resultDiv.innerHTML += 
+          // `<div class="message-container system-message" id="latexresult">
+          //   ${choice.message.content.split('\n').join('<br/>')}
+          //   <button id="copy-btn" onclick="copyToClipboard()">Copy</button>
+          // </div>`
+          
+        })
+
+      resultDiv.scrollTop = resultDiv.scrollHeight;
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
 }
 
-function getLatex(input) {
-  // Call GPT API to convert input text into LaTeX
-  // Replace YOUR_GPT_API_ENDPOINT_HERE with the URL of your GPT API endpoint
-  const url = 'YOUR_GPT_API_ENDPOINT_HERE';
-  const headers = {
-    'Content-Type': 'application/json'
-  };
-  const body = JSON.stringify({
-    "text": input,
-    "model": "YOUR_GPT_MODEL_NAME_HERE"
-  });
-  return fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: body
-  }).then(response => {
-    return response.json();
-  }).then(data => {
-    return data?.latex;
-  }).catch(error => {
-    console.error(error);
-    alert('Error occurred while converting LaTeX.');
-  });
-}
-
-function download() {
-  const image = document.querySelector('#output img');
-  const link = document.createElement('a');
-  link.href = image.src;
-  link.download = 'math_expression.png';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+function copyToClipboard() {
+  const resultDiv = document.getElementById('latexresult');
+  const range = document.createRange();
+  range.selectNode(resultDiv);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  
+  const message = 'Copied to clipboard!';
+  const successMessage = message.replace('copy', '');
+  alert(successMessage);
 }
